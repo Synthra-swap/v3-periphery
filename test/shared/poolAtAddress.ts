426b9dbdc4a46972b545ba2cae9f07c37484c6e4
jacko06v <jacomosconi@gmail.com>
2026-03-20 18:51:19 +0100

    fix

--- test/shared/poolAtAddress.ts
+++
@@ -1,7 +0,0 @@
-import { abi as POOL_ABI } from '@synthra-swap/v3-core/artifacts/contracts/SynthraV3Pool.sol/SynthraV3Pool.json'
-import { Contract, Wallet } from 'ethers'
-import { ISynthraV3Pool } from '../../typechain'
-
-export default function poolAtAddress(address: string, wallet: Wallet): ISynthraV3Pool {
-  return new Contract(address, POOL_ABI, wallet) as ISynthraV3Pool
-}
